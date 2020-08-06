c61720609bde0b3d20116df159e2027388034a28
Prasad <prasadautomationlabs@gmail.com>
2020-08-06 23:07:56 +0530

    Prasad 06-Aug-2020

--- Demo/src/main/java/com/demo/Base/base.java
+++ Demo/src/main/java/com/demo/Base/base.java
@@ -10,10 +10,13 @@ import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.chrome.ChromeDriver;
 	import org.openqa.selenium.firefox.FirefoxDriver;
 	import org.openqa.selenium.ie.InternetExplorerDriver;
-	import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import com.demo.actiondriver.Action;
+import com.demo.utility.ExtentManager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -22,12 +25,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 		public static WebDriver driver;
 
 		@BeforeSuite
-		public void beforesuite() {
-			DOMConfigurator.configure("log4j.xml");
-		}
-		
-		@BeforeTest
 		public void loadConfig() {
+			ExtentManager.setExtent();
+			DOMConfigurator.configure("log4j.xml");
 			
 			try {
 				prop = new Properties();
@@ -41,6 +41,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 				e.printStackTrace();
 			}
 		}
+		
 		public static void launchApp() {
 			String browserName = prop.getProperty("browser");
 			if (browserName.equalsIgnoreCase("Chrome")) {
@@ -60,7 +61,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 			driver.get(prop.getProperty("url"));
 		
 		}
-
+		@AfterSuite
+		public void afterSuite() {
+			ExtentManager.endReport();
+		}
 }
 
 
