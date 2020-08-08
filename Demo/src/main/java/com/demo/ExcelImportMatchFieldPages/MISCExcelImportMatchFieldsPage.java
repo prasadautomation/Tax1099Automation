ed9a7acde68fd15f6e54da3173c3ef10068976d0
Prasad <prasadautomationlabs@gmail.com>
2020-08-08 10:43:44 +0530

    Prasad 08 August 2020 first commit 10:43AM

---
+++ Demo/src/main/java/com/demo/ExcelImportMatchFieldPages/MISCExcelImportMatchFieldsPage.java
@@ -0,0 +1,219 @@
+package com.demo.ExcelImportMatchFieldPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.demo.Base.base;
+
+public class MISCExcelImportMatchFieldsPage extends base{
+
+	//PAYER's Name, Street Address, City, State, Zip code, and Telephone no.
+	@FindBy(xpath="")
+	WebElement PPayerType;
+	
+	@FindBy(xpath="")
+	WebElement PFirstName;
+	
+	@FindBy(xpath="")
+	WebElement PMxpathdleName;
+	
+	@FindBy(xpath="")
+	WebElement PLastName;
+	
+	@FindBy(xpath="")
+	WebElement PSuffix;
+	
+	@FindBy(xpath="")
+	WebElement PAddress;
+	
+	@FindBy(xpath="")
+	WebElement PZipcodeorForeignPostalcode;
+	
+	@FindBy(xpath="")
+	WebElement PLine2;
+	
+	@FindBy(xpath="")
+	WebElement PCountry;
+	
+	@FindBy(xpath="")
+	WebElement PCity;
+	
+	@FindBy(xpath="")
+	WebElement PPhoneNumber;
+	
+	@FindBy(xpath="")
+	WebElement PState;
+	
+	@FindBy(xpath="")
+	WebElement PEmailAddress1;
+	
+	@FindBy(xpath="")
+	WebElement PStatexpath;
+	
+	@FindBy(xpath="")
+	WebElement PayerTINType;
+	
+	@FindBy(xpath="")
+	WebElement PayerTIN;
+	
+	@FindBy(xpath="")
+	WebElement RecipientTINType;
+	
+	@FindBy(xpath="")
+	WebElement RecipientTIN;
+	
+	//RECIPIENT's Name, Street Address, City, State, Zip Code, and Telephone no.
+	
+	@FindBy(xpath="")
+	WebElement RecipientType;
+	
+	@FindBy(xpath="")
+	WebElement AttentionTO;
+	
+	@FindBy(xpath="")
+	WebElement RFirstName;
+	
+	@FindBy(xpath="")
+	WebElement RMxpathdleName;
+	
+	@FindBy(xpath="")
+	WebElement RLastName;
+	
+	@FindBy(xpath="")
+	WebElement RSuffix;
+	
+	@FindBy(xpath="")
+	WebElement RAddress1;
+	
+	@FindBy(xpath="")
+	WebElement RZipcodeorForeignPostalcode;
+	
+	@FindBy(xpath="")
+	WebElement RAddress2;
+	
+	@FindBy(xpath="")
+	WebElement RCountry;
+	
+	@FindBy(xpath="")
+	WebElement RCity;
+	
+	@FindBy(xpath="")
+	WebElement RPhoneNumber;
+	
+	@FindBy(xpath="")
+	WebElement RState;
+	
+	@FindBy(xpath="")
+	WebElement REmailAddress;
+	//AccountNumber
+	@FindBy(xpath="")
+	WebElement AccountNumber;
+	
+	@FindBy(xpath="")
+	WebElement FATCAfilingRequirement;
+	
+	@FindBy(xpath="")
+	WebElement Section15a409Adeferals;
+	
+	@FindBy(xpath="")
+	WebElement Section15b409Aincome;
+	
+	//16. State tax withheld
+	@FindBy(xpath="")
+	WebElement Statetaxwithheld16a;
+	
+	@FindBy(xpath="")
+	WebElement Statetaxwithheld16b;
+	
+	//17. State / Payer's state no.
+	@FindBy(xpath="")
+	WebElement State17a;
+	
+	@FindBy(xpath="")
+	WebElement State17b;
+	
+	@FindBy(xpath="")
+	WebElement StateNo17a;
+	
+	@FindBy(xpath="")
+	WebElement StateNo17b;
+	
+	//18. State income
+	@FindBy(xpath="")
+	WebElement StateIncome18a;
+	
+	@FindBy(xpath="")
+	WebElement StateIncome18b;
+	
+	//Box Amounts
+	@FindBy(xpath="")
+	WebElement Box1;
+	
+	@FindBy(xpath="")
+	WebElement Box2;
+	
+	@FindBy(xpath="")
+	WebElement Box3;
+	
+	@FindBy(xpath="")
+	WebElement Box4;
+	
+	@FindBy(xpath="")
+	WebElement Box5;
+	
+	@FindBy(xpath="")
+	WebElement Box6;
+	
+	@FindBy(xpath="")
+	WebElement Box7;
+	
+	@FindBy(xpath="")
+	WebElement Box8;
+	
+	@FindBy(xpath="")
+	WebElement Box9;
+	
+	@FindBy(xpath="")
+	WebElement Box10;
+	
+	@FindBy(xpath="")
+	WebElement Box13;
+	
+	@FindBy(xpath="")
+	WebElement Box14;
+	
+	@FindBy(xpath="")
+	WebElement ChangeTaxYear;
+	
+	@FindBy(xpath="")
+	WebElement TaxYearDropdown;
+	
+	@FindBy(xpath="")
+	WebElement OMBNumber;
+	
+	@FindBy(xpath="")
+	WebElement Next;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Demo/src/main/java/com/demo/Importforms/form5498ImportPage.java
@@ -0,0 +1,7 @@
+package com.demo.Importforms;
+
+import com.demo.Base.base;
+
+public class form5498ImportPage extends base{
+
+}
