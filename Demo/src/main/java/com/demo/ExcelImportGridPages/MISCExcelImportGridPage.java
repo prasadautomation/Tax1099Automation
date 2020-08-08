9a33fa4a82c1cbe12d44f63f8eeef6aa13458ba8
Prasad <prasadautomationlabs@gmail.com>
2020-08-08 11:43:48 +0530

    Prasad Commit 3 08 August 2020 11:43AM

--- Demo/src/main/java/com/demo/ExcelImportGridPages/MISCExcelImportGridPage.java
+++ Demo/src/main/java/com/demo/ExcelImportGridPages/MISCExcelImportGridPage.java
@@ -1,7 +1,56 @@
 package com.demo.ExcelImportGridPages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.demo.Base.base;
+import com.demo.FormsLeftmenu.SubmitForms;
+import com.demo.actiondriver.Action;
 
 public class MISCExcelImportGridPage extends base{
 
+	@FindBy(xpath="//button[@class='multiselect dropdown-toggle btn btn-default']")
+	WebElement PayerDropdown;
+	
+	@FindBy(xpath="//label[contains(text(),'Select all')]")
+	WebElement PayerDropdownSelectAll;
+	
+	@FindBy(xpath="//div[@class='payer_txtfields']//span")
+	WebElement Taxyear;
+	
+	@FindBy(xpath="//input[@id='selectAllCheckbox']")
+	WebElement SelectAll;
+	
+	@FindBy(xpath="//select[@id='ddlBox']")
+	WebElement ApplytoselectedRecipients;
+	
+	@FindBy(xpath="//input[@id='btnBack']")
+	WebElement Back;
+	
+	@FindBy(xpath="//input[@id='btnbill']")
+	WebElement Next;
+	
+	public MISCExcelImportGridPage() {
+		PageFactory.initElements(driver, this);
+	}	
+	public SubmitForms ClickNext() throws Throwable {
+		Action action = new Action();
+		action.click(driver, Next);
+		return new SubmitForms();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
